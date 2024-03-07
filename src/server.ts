ad86749b9e00ce7a6fbf960772d940858d4b12f2
scobru <scobru1988@gmail.com>
2024-03-07 18:14:48 +0100

    add hello

--- src/server.ts
+++ src/server.ts
@@ -45,8 +45,7 @@ const CONFIGURATIONS: Configurations = {
   networks: NETWORKS,
 };
 
-const TOKENS_URL = "https://tokens.uniswap.org";
-const TOKENS_URL2 = "https://gateway.ipfs.io/ipns/tokens.uniswap.org";
+const TOKENS_URL = "https://gateway.ipfs.io/ipns/tokens.uniswap.org";
 
 app.use(express.json());
 
@@ -68,21 +67,6 @@ app.get("/:chainId/uni-v3/tokens", async (req, res) => {
 
     res.json(filteredTokens);
   } catch (error) {
-    try {
-      const response = await fetch(TOKENS_URL2);
-      const data = await response.json();
-      const { chainId } = req.params;
-
-      // Filter tokens by chainId if needed, for example, chainId 137 (Polygon)
-      const filteredTokens = data.tokens.filter(
-        (token: { chainId: number }) => token.chainId === Number(chainId)
-      );
-
-      res.json(filteredTokens);
-    } catch (error) {
-      res.status(500).json({ message: "Failed to fetch tokens", error: error });
-    }
-
     res.status(500).json({ message: "Failed to fetch tokens", error: error });
   }
 });
