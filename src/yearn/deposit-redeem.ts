5f74fb515f9641b59d5e6fcfc3c3909e07215807
scobru <scobru1988@gmail.com>
2024-03-12 00:46:09 +0100

    Aggiornato il codice per correggere bug e migliorare le prestazioni.

--- src/yearn/deposit-redeem.ts
+++ src/yearn/deposit-redeem.ts
@@ -145,6 +145,10 @@ export async function depositToYearn(
     Calldatas.push(depositCalldata);
     TokensReturn.push(vault.address);
 
+    console.log("::API:: Approvals", Approvals.length);
+    console.log("::API:: Calldatas", Calldatas.length);
+    console.log("::API:: TokensReturn", TokensReturn.length);
+
     return {
       Approvals,
       Calldatas,
@@ -155,12 +159,176 @@ export async function depositToYearn(
   }
 }
 
+export async function depositToYearnBatched(
+  deposits: Array<{
+    wallet: ethers.Wallet;
+    tokenAddr: string;
+    pool: string;
+    amount: BigNumber;
+    receiver: string;
+    chainId: string;
+  }>
+): Promise<{
+  Approvals: Array<any>;
+  Calldatas: Array<any>;
+  TokensReturn: Array<string>;
+}> {
+  let Approvals = [];
+  let Calldatas = [];
+  let TokensReturn = [];
+
+  for (let i = 0; i < deposits.length; i++) {
+    const pool = deposits[i].pool;
+    const amount = deposits[i].amount;
+    const wallet = deposits[i].wallet;
+    const provider = new ethers.providers.JsonRpcProvider(
+      NETWORKS[deposits[0].chainId]
+    );
+    const receiver = deposits[i].receiver;
+    const tokenAddr = deposits[i].tokenAddr;
+    const chainId = deposits[i].chainId;
+    const token = new ethers.Contract(tokenAddr, ERC20ABI, wallet);
+    const vault = new ethers.Contract(pool, YEARN_VAULT_ABI, wallet);
+    const tokenBalance = await token.balanceOf(receiver);
+    const infraRouter = String(INFRA[chainId].ROUTER);
+    const InfraRouterContract = new ethers.Contract(
+      infraRouter,
+      routerAbi,
+      wallet
+    );
+    const agentAddress = await InfraRouterContract?.getAgentAddress(receiver);
+    const gasLimit = 8000000;
+
+    if (tokenBalance.lt(amount)) {
+      throw new Error("::API:: Insufficient balance");
+    }
+
+    const allowanceAgent = await token?.allowance(receiver, agentAddress);
+    console.log("::API:: Agent Address", agentAddress);
+    console.log(
+      "::API:: Allowance for Agent",
+      Number(allowanceAgent).toString()
+    );
+
+    // Sender Approval
+    // -------------------------------------------------------------------------
+
+    if (allowanceAgent.lt(amount)) {
+      console.log("::API:: Approving Agent Address");
+      const approveData = token.interface.encodeFunctionData("approve", [
+        agentAddress,
+        ethers.constants.MaxUint256,
+      ]);
+
+      const approvalCalldata = {
+        to: token.address,
+        value: 0,
+        data: approveData,
+        gasLimit: gasLimit,
+        gasPrice: await provider.getGasPrice(),
+      };
+
+      const found = Approvals.find(
+        (item) => item.to === token.address && item.data === approveData
+      );
+
+      if (!found) {
+        Approvals.push(approvalCalldata);
+      }
+    } else {
+      console.log("::API:: No need to approve Agent Address");
+    }
+
+    const allowanceYearn = await token?.allowance(agentAddress, pool);
+
+    // Agents Calldatas
+    // -------------------------------------------------------------------------
+    if (allowanceYearn.lt(amount)) {
+      console.log("::API:: Approving Yearn Vault");
+      const approveData = token.interface.encodeFunctionData("approve", [
+        pool,
+        ethers.constants.MaxUint256,
+      ]);
+
+      const approvalCalldata = {
+        to: token.address,
+        value: 0,
+        data: approveData,
+        gasLimit: gasLimit,
+        gasPrice: await provider.getGasPrice(),
+      };
+
+      const found = Approvals.find(
+        (item) => item.to === token.address && item.data === approveData
+      );
+
+      if (!found) {
+        Calldatas.push(approvalCalldata);
+      }
+    } else {
+      console.log("::API:: No need to approve Yearn Vault");
+    }
+
+    // Transfer From
+    // -------------------------------------------------------------------------
+    console.log("::API:: Transfering from sender to agent");
+    const transferFromData = token.interface.encodeFunctionData(
+      "transferFrom",
+      [receiver, agentAddress, amount]
+    );
+
+    const transferFromCalldata = {
+      to: token.address,
+      value: 0,
+      data: transferFromData,
+      gasLimit: gasLimit,
+      gasPrice: await provider.getGasPrice(),
+    };
+
+    const found = Calldatas.find(
+      (item) => item.to === token.address && item.data === transferFromData
+    );
+
+    if (!found) {
+      Calldatas.push(transferFromCalldata);
+    }
+
+    // Deposit to Yearn
+    // -------------------------------------------------------------------------
+    const depositData = vault.interface.encodeFunctionData("deposit", [
+      amount,
+      agentAddress,
+    ]);
+
+    const depositCalldata = {
+      to: pool,
+      value: 0,
+      data: depositData,
+      gasLimit: gasLimit,
+      gasPrice: await provider.getGasPrice(),
+    };
+
+    Calldatas.push(depositCalldata);
+    TokensReturn.push(vault.address);
+  }
+
+  console.log("::API:: Approvals", Approvals.length);
+  console.log("::API:: Calldatas", Calldatas.length);
+  console.log("::API:: TokensReturn", TokensReturn.length);
+
+  return {
+    Approvals,
+    Calldatas,
+    TokensReturn,
+  };
+}
+
 export async function redeemFromYearn(
   wallet: ethers.Wallet,
   pool: string,
   amount: BigNumber,
   receiver: string,
-  chainId: number
+  chainId: string
 ) {
   try {
     const provider = new ethers.providers.JsonRpcProvider(NETWORKS[chainId]);
@@ -292,9 +460,10 @@ export async function redeemFromYearn(
 
     const asset = await vault.asset();
     TokensReturn.push(asset);
-    console.log("::API Redeem:: Approvals", Approvals);
-    console.log("::API Redeem:: Calldatas", Calldatas);
-    console.log("::API Redeem:: TokensReturn", TokensReturn);
+
+    console.log("::API:: Approvals", Approvals.length);
+    console.log("::API:: Calldatas", Calldatas.length);
+    console.log("::API:: TokensReturn", TokensReturn.length);
 
     return {
       Approvals,
@@ -304,6 +473,169 @@ export async function redeemFromYearn(
   } catch (e) {
     console.log(e);
   }
+}
+
+export async function redeemFromYearnBatched(
+  redeems: Array<{
+    wallet: ethers.Wallet;
+    pool: string;
+    amount: BigNumber;
+    receiver: string;
+    chainId: string;
+  }>
+): Promise<{
+  Approvals: Array<any>;
+  Calldatas: Array<any>;
+  TokensReturn: Array<string>;
+}> {
+  let Approvals = [];
+  let Calldatas = [];
+  let TokensReturn = [];
+
+  for (let i = 0; i < redeems.length; i++) {
+    const pool = redeems[i].pool;
+    const amount = redeems[i].amount;
+    const wallet = redeems[i].wallet;
+    const provider = new ethers.providers.JsonRpcProvider(
+      NETWORKS[redeems[0].chainId]
+    );
+    const receiver = redeems[i].receiver;
+    const chainId = redeems[i].chainId;
+    const vault = new ethers.Contract(
+      pool,
+      YEARN_VAULT_ABI as ContractInterface,
+      wallet
+    );
+    const vaultBalance = await vault.balanceOf(receiver);
+    const gasLimit = 8000000;
+
+    if (vaultBalance.lt(amount)) {
+      throw new Error("::API:: Insufficient balance");
+    }
+
+    const infraRouter = String(INFRA[chainId].ROUTER);
+    const InfraRouterContract = new ethers.Contract(
+      infraRouter,
+      routerAbi,
+      wallet
+    );
+    const agentAddress = await InfraRouterContract?.getAgentAddress(receiver);
+
+    const allowanceAgent = await vault?.allowance(receiver, agentAddress);
+
+    // Allowance for Agent
+    // -------------------------------------------------------------------------
+    if (allowanceAgent.lt(amount)) {
+      console.log("::API:: Approving agent address");
+      const approveData = vault.interface.encodeFunctionData("approve", [
+        agentAddress,
+        ethers.BigNumber.from(2).pow(256).sub(1),
+      ]);
+
+      const approvalCalldata = {
+        to: vault.address,
+        value: 0,
+        data: approveData,
+        gasLimit: gasLimit,
+        gasPrice: await provider.getGasPrice(),
+      };
+
+      const found = Approvals.find(
+        (item) => item.to === vault.address && item.data === approveData
+      );
+
+      if (!found) {
+        Approvals.push(approvalCalldata);
+      }
+    } else {
+      console.log("::API:: No need to approve agent address");
+    }
+
+    const allowanceAgentYearn = await vault?.allowance(agentAddress, pool);
+
+    // Allowance for Yearn Vault
+    // -------------------------------------------------------------------------
+    if (allowanceAgentYearn.lt(amount)) {
+      const approveData = vault.interface.encodeFunctionData("approve", [
+        pool,
+        ethers.BigNumber.from(2).pow(256).sub(1),
+      ]);
+
+      const approvalCalldata = {
+        to: vault.address,
+        value: 0,
+        data: approveData,
+        gasLimit: gasLimit,
+        gasPrice: await provider.getGasPrice(),
+      };
+
+      const found = Approvals.find(
+        (item) => item.to === vault.address && item.data === approveData
+      );
+
+      if (!found) {
+        Calldatas.push(approvalCalldata);
+      }
+    } else {
+      console.log("::API:: No need to approve Yearn Vault");
+    }
+
+    // Transfer From
+    // -------------------------------------------------------------------------
+    const transferFromData = vault.interface.encodeFunctionData(
+      "transferFrom",
+      [receiver, agentAddress, amount]
+    );
+
+    const transferFromCalldata = {
+      to: vault.address,
+      value: 0,
+      data: transferFromData,
+      gasLimit: gasLimit,
+      gasPrice: await provider.getGasPrice(),
+    };
+
+    const found = Calldatas.find(
+      (item) => item.to === vault.address && item.data === transferFromData
+    );
+
+    if (!found) {
+      Calldatas.push(transferFromCalldata);
+    }
+
+    // Redeem
+    // -------------------------------------------------------------------------
+    const redeemData = vault.interface.encodeFunctionData(
+      "redeem(uint256,address,address,uint256)",
+      [amount, agentAddress, agentAddress, BigNumber.from(200)]
+    );
+
+    const redeemCalldata = {
+      to: pool,
+      value: 0,
+      data: redeemData,
+      gasLimit: gasLimit,
+      gasPrice: await provider.getGasPrice(),
+    };
+
+    const foundRedeem = Calldatas.find(
+      (item) => item.to === pool && item.data === redeemData
+    );
+
+    if (!foundRedeem) Calldatas.push(redeemCalldata);
+
+    const asset = await vault.asset();
+    TokensReturn.push(asset);
+  }
+  console.log("::API:: Approvals", Approvals.length);
+  console.log("::API:: Calldatas", Calldatas.length);
+  console.log("::API:: TokensReturn", TokensReturn.length);
+
+  return {
+    Approvals,
+    Calldatas,
+    TokensReturn,
+  };
 }
 
 export async function accuredYearnInterest(
